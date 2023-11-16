5bda6bd8a2a249b786878462c07bf4cb3fe132d5
bvesely <104418796+2ReGul4r@users.noreply.github.com>
2023-11-16 11:13:56 +0100

    Update ProfileEditCard.vue

--- Turnverein/src/components/Cards/ProfileEditCard.vue
+++ Turnverein/src/components/Cards/ProfileEditCard.vue
@@ -5,21 +5,11 @@
       class="trainer_card d-flex flex-column"
       variant="tonal"
     >
-      <v-dialog
-        v-if="datePicker"
-        activator="parent"
-        width="auto"
-      >
-        <v-date-picker
-          v-model="datePickerDate"
-          @click:cancel="datePicker = false"
-          @click:save="saveDatePicker"
-        />
-      </v-dialog>
       <form>
         <v-text-field
-          v-model="getDate"
-          @click:prepend="datePicker = true"
+          v-model="trainer.birthday"
+          @click:prepend="console.log(trainer)"
+          type="date"
           prepend-icon="mdi-cake"
         />
         <v-text-field
@@ -37,52 +27,22 @@
 </template>
 
 <script lang="ts">
-import { VDatePicker } from 'vuetify/labs/VDatePicker'
 import { defineComponent } from "vue";
 import { useUserStore } from "@/store/user";
 import { mapStores } from "pinia";
 import { Trainer } from "../../../types";
 
 export default defineComponent({
-  components: {
-    VDatePicker,
-  },
   name: "ProfileEditCard",
   computed: {
     ...mapStores(useUserStore),
-    getSubTitle() {
-      return this.trainer.username;
-    },
-    getBirthday() {
-      const birthday = new Date(this.trainer.birthday);
-      return birthday.toLocaleDateString();
-    },
-    getDate() {
-      return (new Date(this.trainer.birthday)).toLocaleDateString();
-    }
-  },
-  methods: {
-    saveDatePicker() {
-      this.datePicker = false;
-      console.log(this.datePickerDate)
-      this.trainer.birthday = new Date(this.datePickerDate);
-    }
   },
   mounted() {
     this.trainer = this.userStore.getUserData;
-    console.log(this.trainer);
-  },
-  watch: {
-    test() {
-      console.log(this.test);
-    }
   },
   data() {
     return {
-      test: "" as any,
       trainer: {} as Trainer,
-      datePicker: false,
-      datePickerDate: "" as any,
     };
   },
 });
